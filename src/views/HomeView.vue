ab7191f320f683e8ec088edff9edd6749e1b25ea
jxes6102 <jxes6102@gamil.com>
2023-12-12 10:49:44 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import {useCounterStore} from '../stores/counter'
+const store = useCounterStore()
+
+
+console.log(store.count)
+
+store.increment(3)
+
+console.log(store.count)
+
+console.log(store.doubleCount)
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
