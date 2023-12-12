07f3ae5387705c75b0689f96efc5f1ba7f5cbf25
jxes6102 <jxes6102@gamil.com>
2023-12-12 17:49:53 +0800

    menu - 2

--- src/components/menuView.vue
+++ src/components/menuView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed top-[8vh] left-0 w-[300px] h-[92vh] bg-lime-400">
+    <div class="fixed top-[8vh] left-0 w-[100vw] md:w-[300px] h-[92vh] bg-lime-400">
         <!-- <button @click="isCollapse = !isCollapse">click</button>
         <transition enter-active-class="animate__animated animate__fadeInLeft">
             <div v-if="isCollapse"> test</div>
