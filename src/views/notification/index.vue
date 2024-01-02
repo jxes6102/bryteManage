2c29d3b5fc94af236b5ce3f357ed22f58af4e365
jxes6102 <jxes6102@gamil.com>
2024-01-02 16:14:32 +0800

    簡化選擇集團 - 1

--- src/views/notification/index.vue
+++ src/views/notification/index.vue
@@ -62,94 +62,6 @@
         </div>
 
         <Teleport to="body">
-            <dialogView type="large" layer="1001" @close="cancelGroup" v-if="groupStatus">
-                <template v-slot:title>
-                    <div class="w-full my-[1px] md:my-1 px-2 py-[1px] md:py-1 text-2xl">選擇集團</div>
-                    <div class="line-style w-[100%] text-[#D3D3D3] flex"></div>
-                </template>
-                <template v-slot:message>
-                    <div class="w-full h-[80%] my-[1px] md:my-1 px-2 py-[1px] md:py-1 flex flex-col justify-around items-center">
-                        <div class="w-full my-[1px] md:my-1 px-2 py-[1px] md:py-1">
-                            <el-form class="w-full" :model="groupForm">
-                                <el-form-item>
-                                    <el-col :span="16">
-                                        <el-input placeholder="集團名稱或集團代號" v-model="groupForm.name" />
-                                    </el-col>
-                                    <el-col :span="8">
-                                        <el-button class="mx-1" type="primary" @click="onSubmit">查詢</el-button>
-                                    </el-col>
-                                </el-form-item>
-                            </el-form>
-                        </div>
-                        <div class="line-style w-[100%] text-[#D3D3D3] flex"></div>
-                        <div class="w-[100%] md:w-[95%] h-auto flex flex-wrap justify-center items-center">
-                            <el-table :data="groupTableData" stripe style="width: 100%">
-                                <el-table-column prop="date" label="集團名稱">
-                                    <template #default="scope">
-                                        <el-tooltip
-                                            :disabled="!isMobile"
-                                            class="box-item"
-                                            effect="light"
-                                            :content="scope.row.date"
-                                            placement="top-start"
-                                        >
-                                            <div class="truncate">{{ scope.row.date }}</div>
-                                        </el-tooltip>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column prop="date" label="集團代號">
-                                    <template #default="scope">
-                                        <el-tooltip
-                                            :disabled="!isMobile"
-                                            class="box-item"
-                                            effect="light"
-                                            :content="scope.row.date"
-                                            placement="top-start"
-                                        >
-                                            <div class="truncate">{{ scope.row.name }}</div>
-                                        </el-tooltip>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column prop="date" label="負責人姓名">
-                                    <template #default="scope">
-                                        <el-tooltip
-                                            :disabled="!isMobile"
-                                            class="box-item"
-                                            effect="light"
-                                            :content="scope.row.date"
-                                            placement="top-start"
-                                        >
-                                            <div class="truncate">{{ scope.row.address }}</div>
-                                        </el-tooltip>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column prop="date" label="單位數量">
-                                    <template #default="scope">
-                                        <el-tooltip
-                                            :disabled="!isMobile"
-                                            class="box-item"
-                                            effect="light"
-                                            :content="scope.row.date"
-                                            placement="top-start"
-                                        >
-                                            <div class="truncate">{{ scope.row.address }}</div>
-                                        </el-tooltip>
-                                    </template>
-                                </el-table-column>
-                            </el-table>
-                        </div>
-                        <div class="w-[100%] h-auto flex flex-wrap justify-center items-center">
-                            <el-pagination
-                                small
-                                background
-                                layout="prev, pager, next"
-                                :total="50"
-                                class="mt-1"
-                            />
-                        </div>
-                    </div>
-                </template>
-            </dialogView>
             <dialogView type="large" @close="cancelUser" v-if="userStatus">
                 <template v-slot:title>
                     <div class="w-full my-[1px] md:my-1 px-2 py-[1px] md:py-1 text-2xl">選擇使用者</div>
@@ -267,6 +179,8 @@
                 </template>
             </dialogView>
         </Teleport>
+
+        <choseGroup @close="cancelGroup" v-if="groupStatus"></choseGroup>
     </div>
 </template>
 
@@ -276,6 +190,7 @@ import { ref,computed } from "vue"
 import { useRouter,useRoute } from "vue-router"
 import { useMobileStore } from '@/stores/index'
 import dialogView from "@/components/dialogView.vue"
+import choseGroup from "@/components/choseGroup.vue"
 
 const router = useRouter()
 const route = useRoute()
@@ -287,9 +202,6 @@ const isMobile = computed(() => {
 })
 
 const groupStatus = ref(false)
-const groupForm = ref({
-  name: '',
-})
 
 const form = ref({
   title: '',
@@ -314,34 +226,6 @@ const cancelGroup = () => {
 const cancelUser = () => {
     userStatus.value = false
 }
-
-const groupTableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-]
 
 const userTableData = [
   {
--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -75,109 +75,9 @@
             共 1 筆資料
         </div>
 
+        <choseGroup @close="cancel" v-if="groupStatus"></choseGroup>
+
         <Teleport to="body">
-            <dialogView type="large" @close="cancel" v-if="groupStatus">
-                <template v-slot:title>
-                    <div class="w-full my-[1px] md:my-1 px-2 py-[1px] md:py-1 text-2xl">選擇集團</div>
-                    <div class="line-style w-[100%] text-[#D3D3D3] flex"></div>
-                </template>
-                <template v-slot:message>
-                    <div class="w-full h-[80%] my-[1px] md:my-1 px-2 py-[1px] md:py-1 flex flex-col justify-around items-center">
-                        <div class="w-full my-[1px] md:my-1 px-2 py-[1px] md:py-1">
-                            <el-form class="w-full" :model="groupForm">
-                                <el-form-item>
-                                    <el-col :span="16">
-                                        <el-input placeholder="集團名稱或集團代號" v-model="groupForm.name" />
-                                    </el-col>
-                                    <el-col :span="8">
-                                        <el-button class="mx-1" type="primary" @click="onSubmit">查詢</el-button>
-                                    </el-col>
-                                </el-form-item>
-                            </el-form>
-                        </div>
-                        <div class="line-style w-[100%] text-[#D3D3D3] flex"></div>
-                        <div class="w-[100%] h-auto flex flex-wrap justify-center items-center">
-                            <el-table :data="groupTableData" stripe style="width: 100%">
-                                <el-table-column prop="date" label="集團名稱">
-                                    <template #default="scope">
-                                        <el-tooltip
-                                            :disabled="!isMobile"
-                                            class="box-item"
-                                            effect="light"
-                                            :content="scope.row.date"
-                                            placement="top-start"
-                                        >
-                                            <div class="truncate">{{ scope.row.date }}</div>
-                                        </el-tooltip>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column prop="date" label="集團代號">
-                                    <template #default="scope">
-                                        <el-tooltip
-                                            :disabled="!isMobile"
-                                            class="box-item"
-                                            effect="light"
-                                            :content="scope.row.date"
-                                            placement="top-start"
-                                        >
-                                            <div class="truncate">{{ scope.row.name }}</div>
-                                        </el-tooltip>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column prop="date" label="負責人姓名">
-                                    <template #default="scope">
-                                        <el-tooltip
-                                            :disabled="!isMobile"
-                                            class="box-item"
-                                            effect="light"
-                                            :content="scope.row.date"
-                                            placement="top-start"
-                                        >
-                                            <div class="truncate">{{ scope.row.address }}</div>
-                                        </el-tooltip>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column prop="date" label="單位數量">
-                                    <template #default="scope">
-                                        <el-tooltip
-                                            :disabled="!isMobile"
-                                            class="box-item"
-                                            effect="light"
-                                            :content="scope.row.date"
-                                            placement="top-start"
-                                        >
-                                            <div class="truncate">{{ scope.row.address }}</div>
-                                        </el-tooltip>
-                                    </template>
-                                </el-table-column>
-                            </el-table>
-                        </div>
-                        <div class="w-[100%] h-auto flex flex-wrap justify-center items-center">
-                            <el-pagination
-                                small
-                                background
-                                layout="prev, pager, next"
-                                :total="50"
-                                class="mt-1"
-                            />
-                        </div>
-                    </div>
-                </template>
-                <template v-slot:control>
-                    <div class="line-style w-[100%] text-[#D3D3D3] flex"></div>
-                    <div class="w-full h-auto my-1 px-2 py-1 flex flex-wrap justify-end items-center">
-                        <button
-                            class="min-w-[10%] bg-blue-500 hover:bg-blue-600 text-white font-bold mx-2 py-1 px-2 md:py-2 md:px-3 rounded">
-                            確定
-                        </button>
-                        <button
-                            @click="cancel"
-                            class="min-w-[10%] bg-blue-500 hover:bg-blue-600 text-white font-bold mx-2 py-1 px-2 md:py-2 md:px-3 rounded">
-                            取消
-                        </button>
-                    </div>
-                </template>
-            </dialogView>
             <dialogView @close="cancel" v-if="authorityStatus">
                 <template v-slot:title>
                     <div class="w-full my-[1px] md:my-1 px-2 py-[1px] md:py-1 text-2xl">群組名稱</div>
@@ -220,6 +120,7 @@ import { ref,computed } from "vue"
 import { useRouter,useRoute } from "vue-router"
 import { useMobileStore } from '@/stores/index'
 import dialogView from "@/components/dialogView.vue"
+import choseGroup from "@/components/choseGroup.vue"
 
 const router = useRouter()
 const route = useRoute()
@@ -231,9 +132,6 @@ const isMobile = computed(() => {
 })
 
 const groupStatus = ref(false)
-const groupForm = ref({
-  name: '',
-})
 
 const form = ref({
   name: '',
@@ -260,34 +158,6 @@ const cancel = () => {
 }
 
 const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'Los Angeles',
-  },
-]
-
-const groupTableData = [
   {
     date: '2016-05-03',
     name: 'Tom',
